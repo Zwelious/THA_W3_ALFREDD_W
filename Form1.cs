2e8ece40a9f6bbb1ade0624b781b0caf670a9e96
Zwelious <112923618+Zwelious@users.noreply.github.com>
2023-03-18 00:28:43 +0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace THA_W3_Alfredd_W
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            submit_btn.Enabled = false;
+        }
+
+        private void true_check_CheckedChanged(object sender, EventArgs e)
+        {
+            if (true_check.Checked == true)
+            {
+                submit_btn.Enabled = true;
+            }
+            else
+            {
+                submit_btn.Enabled = false;
+            }
+        }
+
+        private void submit_btn_Click(object sender, EventArgs e)
+        {
+            if(name_text.Text == "" || artist_text.Text == " ")
+            {
+                MessageBox.Show("Enter correct input", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                Form2.form2.label.Text = "Hi, my name is" + name_text.Text + " and my favorite artist is " + artist_text.Text;
+            }
+        }
+
+        private void next_btn_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void name_text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void artist_text_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace THA_W3_Alfredd_W
+{
+    public partial class Form2 : Form
+    {
+        public static Form2 form2;
+        public Label label { get; set; }
+        public Form1 form1 { get; set; }
+        public Form2()
+        {
+            InitializeComponent();
+            form2 = this;
+            label = output_lbl;
+            magic_btn.Enabled = false;
+        }
+
+        private void true2_check_CheckedChanged(object sender, EventArgs e)
+        {
+            if (true2_check.Checked == true && agree_check.Checked == true)
+            {
+                magic_btn.Enabled = true;
+            }
+        }
+
+        private void agree_check_CheckedChanged(object sender, EventArgs e)
+        {
+            if (true2_check.Checked == true && agree_check.Checked == true)
+            {
+                magic_btn.Enabled = true;
+            }
+        }
+        private void magic_btn_Click(object sender, EventArgs e)
+        {
+            
+            if (orange_radio.Checked)
+            { form1.BackColor = Color.Orange; }
+            else if (cyan_radio.Checked)
+            { form1.BackColor = Color.Cyan; }
+            else if (pink_radio.Checked)
+            { form1.BackColor = Color.Pink; }
+            else if (green_radio.Checked)
+            { form1.BackColor = Color.Green; }
+            else if (purp_radio.Checked)
+            { form1.BackColor = Color.Purple; }
+
+            if (blue_radio.Checked)
+            { form1.ForeColor = Color.Blue; }
+            else if (red_radio.Checked)
+            { form1.ForeColor = Color.Red; }
+            else if (white_radio.Checked)
+            { form1.ForeColor = Color.Orange; }
+        }
+    }
+}
